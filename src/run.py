b199a4afb6e3f3a2560e5598b1e984e639d31298
Amirhossein <ahzrn.dev@gmail.com>
2025-10-29 19:29:17 +0330

    test2

--- src/run.py
+++ src/run.py
@@ -1,8 +1,10 @@
-import streamlit as st
-import seaborn as sns
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
 import streamlit as st
+import pandas as pd
+
+
 #login and signup settings
 login_option=st.sidebar.radio('login/signup',('login','signup'))
 if login_option=='login' :
@@ -33,7 +35,8 @@ st.image("PythonToolKit_Banner.png",caption="This is a python project")
 #metric definitions
 col1,col2 ,col3 = st.columns(3)
 col1.metric(label="my website members",value="4000",delta="50")
-col2.metric(label="my telegram groups members",value="300",delta="-10")
+if col2.checkbox("select this") :
+    col2.metric(label="my telegram groups members",value="300",delta="-10")
 col3.metric(label="my instagram folowers",value='5k',delta='1250')
 
 #title settings
@@ -44,6 +47,13 @@ with st.expander('statistics'):
     fig , ax = plt.subplots(1,1,figsize=(10,5))
     sns.histplot(np.random.randn(100),ax=ax)
     st.pyplot(fig)
+    df=pd.DataFrame({
+        "first column": ["1","2","3"],
+        "secomd column": ["4","5","6"],
+    })
+    df
+
+
 #expanders settings
 with st.expander("user profile"):
     col1 , col2 = st.columns(2)
@@ -51,3 +61,21 @@ with st.expander("user profile"):
     col2.text_input("location :")
     st.camera_input('camera input',key= 'camera_input')
 
+
+if st.toggle("On/Off") :
+    st.write ("turn off that boy!!!!")
+
+x=st.slider("selec a value",-50,100,0)
+st.write(x)    
+
+contatct_type=st.selectbox(
+    "whats your favorite device",
+    ("phone","macbook","ipad"),
+)
+st.write(f'your favorite device is {contatct_type}')
+
+contatct_type=st.multiselect(
+    "whats your favorite device",
+    ("phone","macbook","ipad"),
+)
+st.write(f'your favorite device is {contatct_type}')
